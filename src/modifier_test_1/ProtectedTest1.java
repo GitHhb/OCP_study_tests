710dbf6e18b71ec61013693dd5c9925e59a234fb
Leandro <leandro@dagostino.name>
2016-01-13 01:15:59 +0100

    .

---
+++ src/modifier_test_1/ProtectedTest1.java
@@ -0,0 +1,11 @@
+package modifier_test_1;
+
+/**
+ * Created by L on 1/12/2016.
+ */
+public class ProtectedTest1 {
+    public ProtectedTest1 () {}
+    protected String protectedVar = "I'm protected in modifier_test_1";
+    protected void protectedMethod () {System.out.println("protected method in modifier_test_1");}
+
+}
---
+++ src/modifier_test_2/ProtectedTest2.java
@@ -0,0 +1,23 @@
+package modifier_test_2;
+import modifier_test_1.ProtectedTest1;
+
+public class ProtectedTest2 extends ProtectedTest1 {
+    //String protectedVar2 = "var2";
+    protected void protectedMethod2() {}
+    public void methodIn2 () {
+        protectedVar = "I'm protected in modifier_test_2";
+        protectedMethod();
+        ProtectedTest2 p = new ProtectedTest2();
+        p.protectedVar = "hoi";
+        //p.protectedMethod();
+    }
+}
+
+class Unrelated2 {
+    public void unrelatedIn2(){
+        ProtectedTest2 p = new ProtectedTest2();
+        p.protectedVar = "hoi";
+        //p.protectedMethod();
+
+    }
+}
